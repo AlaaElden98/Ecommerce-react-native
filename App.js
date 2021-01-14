ebb1933c1d3a199b8863e848adbce9a773d4dbe3
Alaa Abdelfattah <alaa.abdelfattah16@gmail.com>
2021-01-14 02:25:27 +0200

    Check if user is already registered

--- App.js
+++ App.js
@@ -2,7 +2,16 @@ import React from 'react';
 import {Button, View, Text} from 'react-native';
 import {AppContainer} from './src/navigation';
 import axios from 'axios';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {TOKEN_KEY} from './src/utils/constants';
 export default function App() {
-  return <AppContainer />;
+  const [token, setToken] = React.useState();
+  React.useEffect(() => {
+    AsyncStorage.getItem(TOKEN_KEY).then((val) => {
+      setToken(val);
+      axios.defaults.headers.Authorization = 'Bearer ' + val;
+    });
+  }, []);
+
+  return !!token && <AppContainer isAuthanticated={!!token} />;
 }
