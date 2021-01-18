7aa2402474222958fbf67f732be7f38a3917dd52
Alaa Abdelfattah <alaa.abdelfattah16@gmail.com>
2021-01-18 03:09:21 +0200

    Install reactotronRedux

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,14 @@ import {createStore, combineReducers, applyMiddleware} from 'redux';
 import authReducer from './reducers/authReducer';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
+import Reactotron from '../../ReactotronConfig';
 
 const rootReducer = combineReducers({
   auth: authReducer,
 });
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(thunk)),
+  composeWithDevTools(applyMiddleware(thunk), Reactotron.createEnhancer()),
 );
 
 export default store;
