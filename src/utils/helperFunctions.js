1b71f38a05934ac2204b0e7832dc026b157c4389
Alaa Abdelfattah <alaa.abdelfattah16@gmail.com>
2021-02-07 07:05:38 +0200

    Add launcher icon for android

--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -4,16 +4,6 @@ import store from '../redux/store';
 import {BASE_URL} from './constants';
 import {errorCodeMessageMapper} from './errorCodes';
 
-export const formatTimer = (totalSeconds) => {
-  const seconds = totalSeconds % 60;
-  const minutes = (totalSeconds - seconds) / 60;
-  return appendZero(minutes) + ':' + appendZero(seconds);
-};
-
-const appendZero = (val) => {
-  return val < 10 ? '0' + val : val;
-};
-
 export const getActualPrice = (price, discount) => {
   if (discount) {
     return price - price * discount;
